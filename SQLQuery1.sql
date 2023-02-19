bb55b51213054b71ea049b6eac0735b5ffdfc18b
Ysunil9 <sunil.pcs24@gmail.com>
2023-02-19 21:50:44 +0530

    [SUNIL]:Added Data In To Table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -6,3 +6,9 @@ Id int identity (1,1) primary key,
 Name varchar(200),
 Salary float,
 StartDate date);
+
+Insert into PayrollTable (Name, Salary, StartDate) values
+('Dhoni', 60000.00, '2021-5-26'),
+('Virat', 40000.00, '2021-06-26'),
+('Ashwin',50000.00, '2021-06-09'),
+('Jadeja',30000.00, '2021-03-05');
