af448b1fb536097f29143e1e13fa525864daa6fd
Ysunil9 <sunil.pcs24@gmail.com>
2023-02-21 22:22:38 +0530

    [SUNIL]:adding Gender Column

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -12,8 +12,23 @@ Insert into PayrollTable (Name, Salary, StartDate) values
 ('Virat', 40000.00, '2021-06-26'),
 ('Ashwin',50000.00, '2021-06-09'),
 ('Jadeja',30000.00, '2021-03-05');
+('Mithali',70000.00, '2020-03-05');
+
 
 Select * from PayrollTable
 
 select salary from PayrollTable where Name = 'Dhoni';
 select * from PayrollTable where StartDate between cast ('2021-06-09' as date) and GETDATE();
+
+alter table PayrollTable
+ add Gender char(1);
+update PayrollTable set Gender = 'M' where Id in (1,2,3,4);
+update PayrollTable set Gender = 'F' where Id in (5);
+
+Select * from PayrollTable
+
+
+
+
+
+
