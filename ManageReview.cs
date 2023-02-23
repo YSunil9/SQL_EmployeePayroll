8f79c6dc416d8a627080c941181e6d8fb82809f7
Ysunil9 <sunil.pcs24@gmail.com>
2023-02-23 15:10:49 +0530

    [SUNIL]:Product review list is added

---
+++ ManageReview.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQProductReview
+{
+    public class ManageReview
+    {
+        public void Display(List<ProductReviewModel> reviewList)
+        {
+            Console.WriteLine("Display The FeedBack of The Product\n");
+            //Print products
+            foreach (var feedback in reviewList)
+            {
+                Console.WriteLine($"ProductID: {feedback.ProductId},UserId: {feedback.UserId}Rating: {feedback.Rating},Review: {feedback.Review}");
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace LINQProductReview
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ProductReviewModel> reviewList = new List<ProductReviewModel>()
+        {
+            new ProductReviewModel() { ProductId = 101, UserId = 1, Rating = 4.5, Review = "Good", isLike = true },
+            new ProductReviewModel() { ProductId = 102, UserId = 2, Rating = 4, Review = "Good", isLike = true },
+            new ProductReviewModel() { ProductId = 103, UserId = 3, Rating = 4, Review = "Good", isLike = true },
+            new ProductReviewModel() { ProductId = 104, UserId = 4, Rating = 3.5, Review = "Good", isLike = true },
+            new ProductReviewModel() { ProductId = 105, UserId = 5, Rating = 1.5, Review = "Worst", isLike = false },
+            new ProductReviewModel() { ProductId = 101, UserId = 6, Rating = 1, Review = "No Bad", isLike = false },
+            new ProductReviewModel() { ProductId = 102, UserId = 7, Rating = 2, Review = "Worst", isLike = false },
+            new ProductReviewModel() { ProductId = 103, UserId = 8, Rating = 3, Review = "Ok", isLike = false },
+            new ProductReviewModel() { ProductId = 104, UserId = 9, Rating = 5, Review = "Excellent", isLike = true },
+            new ProductReviewModel() { ProductId = 105, UserId = 10, Rating = 5, Review = "Excellent", isLike = true },
+            new ProductReviewModel() { ProductId = 101, UserId = 11, Rating = 4, Review = "Good", isLike = true },
+            new ProductReviewModel() { ProductId = 102, UserId = 12, Rating = 3.5, Review = "Average", isLike = false },
+            new ProductReviewModel() { ProductId = 103, UserId = 13, Rating = 3, Review = "Ok", isLike = false },
+            new ProductReviewModel() { ProductId = 104, UserId = 14, Rating = 4.5, Review = "Good", isLike = true },
+            new ProductReviewModel() { ProductId = 105, UserId = 15, Rating = 4, Review = "Good", isLike = true },
+            new ProductReviewModel() { ProductId = 101, UserId = 16, Rating = 5, Review = "Good", isLike = true },
+            new ProductReviewModel() { ProductId = 102, UserId = 17, Rating = 2, Review = "Worst", isLike = false },
+            new ProductReviewModel() { ProductId = 103, UserId = 18, Rating = 2.5, Review = "Worst", isLike = false },
+            new ProductReviewModel() { ProductId = 104, UserId = 19, Rating = 2.5, Review = "Worst", isLike = false },
+            new ProductReviewModel() { ProductId = 105, UserId = 20, Rating = 3, Review = "Average", isLike = false },
+            new ProductReviewModel() { ProductId = 101, UserId = 20, Rating = 3, Review = "Average", isLike = false },
+            new ProductReviewModel() { ProductId = 102, UserId = 22, Rating = 2, Review = "Average", isLike = false },
+            new ProductReviewModel() { ProductId = 103, UserId = 23, Rating = 2, Review = "Worst", isLike = false },
+            new ProductReviewModel() { ProductId = 104, UserId = 24, Rating = 4, Review = "Good", isLike = true },
+            new ProductReviewModel() { ProductId = 105, UserId = 25, Rating = 5, Review = "Excellent", isLike = true },
+            };
+            ManageReview obj = new ManageReview();
+            obj.Display(reviewList);
+        }    
+    }
+}
